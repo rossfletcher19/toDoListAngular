550e22fb09f4e8b99e0cf8afa4b828c1c29d537d
Matt Jackson and Ross Fletcher <matt@>
2018-02-19 15:18:30 -0800

    add Angular event bindings for edit Task and isDone

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,13 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <div class="container">
-     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-     <h3>{{currentFocus}}</h3>
-     <ul>
-       <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
-     </ul>
-   </div>
+         <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+         <h3>{{currentFocus}}</h3>
+         <ul>
+           <li (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}}  <button (click)="editTask()">Edit!</button></li>
+         </ul>
   `
 })
 
@@ -24,7 +22,22 @@ export class AppComponent {
     new Task('Begin brainstorming possible JavaScript group projects'),
     new Task('Add README file to last few Angular repos on GitHub')
   ];
+
+  editTask() {
+    alert("You just requested to edit a Task!");
+  }
+
+  isDone(clickedTask: Task) {
+    if(clickedTask.done === true) {
+      alert("This task is done!");
+    } else {
+      alert("This task is not done. Better get to work!")
+    }
+  }
+
+  
 }
+
 export class Task {
   public done: boolean = false;
   constructor(public description: string) {
